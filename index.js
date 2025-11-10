f133ae9a095721bae133755eec01f1d43e2fbdeb
kamrulmiazi <kamrul116522@gmail.com>
2025-11-10 18:58:26 +0600

    mongoDB connect and cars get,post,patch,delete api write

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
@@ -7,12 +9,80 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
+// ================main=================
+app.get("/", (req, res) => {
+  res.send({ massage: "Server is running." });
+});
 
-// ---------------main----------------
-app.get('/', (req, res) => {
-    res.send({massage: "Server is running."})
-})
+// ----------mongoDB connect-----------
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@simple-crud-server.1orocpb.mongodb.net/?appName=simple-crud-server`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+
+    // mongoDB collection and database
+    const database = client.db("CarRental");
+    const carsCollection = database.collection("cars");
+
+    // -------car related api-------
+    // get all cars
+    app.get("/cars", async (req, res) => {
+      const result = await carsCollection.find().toArray();
+      res.send(result);
+    });
+
+    // get specific car
+    app.get("/cars/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await carsCollection.findOne(query);
+      res.send(result);
+    });
+
+    // car post api
+    app.post("/cars", async (req, res) => {
+      const doc = req.body;
+      const result = await carsCollection.insertMany(doc);
+      res.send.apply(result);
+    });
+
+    // car edit api
+    app.patch("/cars/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const update = {
+        $set: req.body,
+      };
+      const result = await carsCollection.updateOne(query, update);
+      res.send(result);
+    });
+
+    // car delete api
+    app.delete("/cars/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await carsCollection.deleteOne(query);
+      res.send(result);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
-    console.log('Server running on port:', port);
-})
+  console.log("Server running on port:", port);
+});
